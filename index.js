7a5094e02b5d1e601868007e50b1a34d67d4dada
Uluk <tarielevuluk@gmail.com>
2023-04-11 23:07:51 +0600

    home work

---
+++ index.js
@@ -0,0 +1,564 @@
+"use strict";
+
+
+//#1
+// alert('Hello world');
+
+//Переменые
+
+// let num = 123;
+// alert(num);
+
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+
+
+// let a = 1, b = 2, c = 3;
+
+
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = 3;
+
+
+// let a;
+// a = 33;
+// a = 333;
+// alert(a);
+
+// Математические операции с числами в JavaScript
+
+// let a = 1 + 2 + 3;
+// alert(a);
+
+// let a = 10;
+// let b = 2;
+// let c = a + b;
+// alert(c);
+
+
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+// alert(result);
+
+
+// let a = 5, b = 5, c = 5;
+// let result = a + b + c;
+// alert(result);
+
+// let a = 10;
+// let b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c * d;
+// alert(result); 
+
+
+// Приоритет математических операций в JavaScript
+
+// let a = 5 + 5 * 3;
+// alert(a);
+
+// let a = 8 / 2 * 2;
+// alert(a);
+
+// let a = (2 + 3) * (2 + 3);
+// alert(a);
+
+// let a = (2 + 3) * 2 + 3;
+// alert(a);
+
+// let a = 2 * (2 + 4 * (3 + 1));
+// alert(a);
+
+// let a = 2 * 8 / 4;
+// alert(a);
+
+// let a = (2 * 8) / 4;
+// alert(a);
+
+// let a = 2 * (8 / 4);
+// alert(a);
+
+
+// Дроби в JavaScript
+
+// let a = 1.5;
+// let b = 0.75;
+// let c = a + b;
+// alert(c);
+
+// Отрицательные числа в JavaScript
+
+// let a = -100;
+// alert(a);
+
+// let a = 333;
+// let b = -a;
+// alert(b);
+
+// Остаток от деления в JavaScript
+
+// let a = 13;
+// let b = 5;
+// alert(a % b); 
+
+
+// Возведение в степень в JavaScript
+
+
+// let a = 2;
+// alert(a ** 10);
+
+// let a = 3 * 2 ** 3;
+// alert(a);
+
+// let a = (3 * 2) ** 3;
+// alert(a);
+
+// let a = 3 * 2 ** (3 + 1);
+// alert(a);
+
+// let a = 2 ** 3 * 3;
+// alert(a);
+
+// let a = 3 * 2 ** 3 * 3;
+// alert(a);
+
+
+
+// Строки в JavaScript
+
+// let a = 'tar';
+// alert(a);
+
+
+
+//Сложение строк в JavaScrip
+
+// let str = '!!!';
+// alert(str);
+
+// let a = 'Java';
+// let  b = 'Script';
+// alert(a + b);
+
+// let a = 'hello';
+// let  b = ' world';
+// alert(a + b);
+
+
+
+// Длина строки в JavaScript
+
+// let a = 'sddgsererr';
+// alert(a.length);
+
+
+// Шаблонные строки в JavaScript
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc'; 
+// alert(txt);
+
+
+// Специальные значения в JavaScript
+
+// let a;
+// alert(a);
+
+// let a = null;
+// alert(a);
+
+// let a = true;
+// alert(a);
+
+// let a = false;
+// alert(a);
+
+// let a = 433;
+// let b = 'dfsdf';
+// alert(a * b);
+
+// alert(10 / 0);
+
+
+
+// Работа с консолью в JavaScript
+
+// console.log(123);
+
+// let a = 32432;
+// let b = 342432;
+// console.log(a, b);
+
+// let a = 32432;
+// let b = 342432;
+// let c = 5555;
+// console.log(a, b, c);
+
+// console.log('23123');
+
+// console.log(23123);
+
+// alert(aaa);
+
+
+// Автоматическое преобразование типов данных
+
+// let a = '5' + '2';
+// alert(a);
+
+// let a = '5' + 2;
+// alert(a);
+
+// let a = 5 + '2';
+// alert(a);
+
+// let a = 5 + 2;
+// alert(a);
+
+// let a = '5' * '2';
+// alert(a);
+
+// let a = '5' - '2';
+// alert(a);
+
+// let a = '5' / '2';
+// alert(a);
+
+// let a = '5' % '2';
+// alert(a);
+
+// let a = '5s' * '2';
+// alert(a);
+
+// let a = '5s' + '2';
+// alert(a);
+
+// let a = (-'5') + (-'2');
+// alert(a);
+
+// let a = '5' * 1 + '2' * 1;
+// alert(a);
+
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+
+// let a = '' + 3 + 1;
+// alert(a);
+
+
+// Преобразование к числу в JavaScript
+
+// let a = Number('3');
+// let b = Number('5');
+// alert(a * b);
+
+// alert( Number('2') + Number('3') );
+
+// alert( 2 + Number('3') );
+
+// alert( '2' + Number('3') );
+
+
+// let a = '2';
+// let b = '3';
+// alert(+a + +b);
+
+// Выделение чисел в JavaScript
+
+// let a = parseFloat('5px');
+// let b = parseFloat('6px');
+// alert(b);
+// alert(a);
+
+// let a = parseFloat('5.5px');
+// let b = parseFloat('6.25px');
+// alert(b);
+// alert(a);
+
+
+// let a = parseFloat('5.5px');
+// let b = parseFloat('6.25px');
+// let c = a + b;
+// alert(c + 'px')
+
+// Преобразование к строке в JavaScript
+
+
+// let a = 1;
+// let b = 2;
+// alert(String(a) + String(b));
+
+// let a = 3453445;
+// alert(String(a).length);
+
+
+// let a = 12345;
+// let b = 12345;
+// alert(String(a).length + String(b).length);
+
+
+// Преобразование логического типа в JavaScript
+
+// alert(true + 3);
+
+// alert(true + true);
+
+// alert(true - true);
+
+// alert(true - true);
+
+// alert('1' + true);
+
+// alert( String(true) + 1 );
+
+// alert( String(true) + Number(true)); 
+
+
+// Преобразование к логическому типу в JavaScript
+
+// let test = Boolean(3);
+// alert(test);
+
+// let test = Boolean(0);
+// alert(test);
+
+// let test = Boolean(-1);
+// alert(test);
+
+// let test = Boolean(-0);
+// alert(test);
+
+// let test = Boolean(+0);
+// alert(test);
+
+// let test = Boolean('abc');
+// alert(test);
+
+// let test = Boolean('');
+// alert(test);
+
+// let test = Boolean('0');
+// alert(test);
+
+// let test = Boolean(true);
+// alert(test);
+
+// let test = Boolean(false);
+// alert(test);
+
+// let test = Boolean('true');
+// alert(test);
+
+// let test = Boolean('false');
+// alert(test);
+
+// let test = Boolean(null);
+// alert(test);
+
+// let test = Boolean('null');
+// alert(test);
+
+// let test = Boolean(undefined);
+// alert(test);
+
+// let test = Boolean('undefined');
+// alert(test);
+
+// let test = Boolean(NaN);
+// alert(test);
+
+// let test = Boolean('NaN');
+// alert(test);
+
+// let test = Boolean(3 * 'abc');
+// alert(test);
+
+// let test = Boolean(Infinity);
+// alert(test);
+
+// let test = Boolean(1 / 0);
+// alert(test);
+
+
+
+// Получение символов строки на JavaScript
+
+// let a = 'abcde'
+// alert(a[0]);
+// alert(a[1]);
+// alert(a[2]);
+// alert(a[4]);
+
+
+// let a = 'abcde'
+// alert(a[4] + a[3] + a[2] + a[1] + a[0]);
+
+// let str = 'abcde';
+// let num = 1;
+// alert(str[num]);
+
+
+
+// Последний символ строки в JavaScript
+
+// let a = 'abcde';
+// let b = a.length - 1;
+// alert(a[b]);
+
+// let a = 'abcde';
+// let b = a.length - 2;
+// alert(a[b]);
+
+// let a = 'abcde';
+// let b = a.length - 3;
+// alert(a[b]);
+
+
+// Строки с цифрами в JavaScript
+
+// let a = '12345'; 
+// alert(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]));
+
+// let a =  String(12345); 
+// alert(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]))
+
+// let a =  String(12345); 
+// alert(a[0] * a[1] * a[2] * a[3] * a[4]);
+
+// let a =  String(12345); 
+// alert(a[4] + a[3] + a[2] + a[1] + a[0]);
+
+
+//Сокращенные операции в JavaScript
+
+// let num = 1;
+// num = num + 1;
+// num = num + 1;
+// alert(num);
+
+// let num = 1;
+// num = num + 2;
+// num = num + 3;
+// alert(num);
+
+// let num = 47;
+// num += num + 7;
+// num -= num - 18;
+// num *= num * 10;
+// num /= num / 15;
+// alert(num);
+
+
+// Операции инкремента и декремента в JavaScript
+
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+// let num = 3;
+// alert(++num);
+
+// let num = 3;
+// alert(num++);
+
+// let num = 3;
+// alert(--num);
+
+// let num = 3;
+// alert(num--);
+
+// let num1 = 3;
+// let num2 = ++num1;
+// alert(num1);
+// alert(num2);
+
+// let num1 = 3;
+// let num2 = num1++;
+// alert(num1);
+// alert(num2);
+
+// let num1 = 3;
+// let num2 = --num1;
+// alert(num1);
+// alert(num2);
+
+// let num1 = 3;
+// let num2 = --num1;
+// alert(num1);
+// alert(num2);
+
+// let num1 = 3;
+// num1++;
+// let num2 = num1--;
+// alert(num1++);
+// alert(--num2);
+
+
+// Неточные вычисления в JavaScript
+
+// alert(0.1 * 0.2);
+
+// alert(0.3 - 0.1);
+
+
+// Функция prompt в JavaScript
+
+
+// let age = prompt('Ваш возраст');
+// alert(age);
+
+
+// alert('Ваш возраст: ' + prompt('Ваш возраст'));
+ 
+
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+
+// alert(Number(num1) + Number(num2));
+
+// let a = prompt('Выведите на экран площадь введенного квадрата.');
+
+// let a = prompt('ввелите стороны прямо')
+// let b = prompt('ввелите стороны прямо')
+//  alert(Number(a) + Number(b))
+
+
+// Вывод текста в документ в JavaScript
+
+// let str = 'text';
+// document.write('<b>' + str + '</b>');
+
+
+// document.write('text1<br>');
+// document.write('text2<br>');
+// document.write('text3<br>');
+
+// document.write('sfsfsf')
+
+// document.write('<i>sfsfsf</i>');
+
+// let str = 'text';
+// document.write('<i>' + str + '</i>');
+
+// document.write('1<br>')
+// document.write('2<br>')
+// document.write('3<br>')
+// document.write('4<br>')
+
+
+// Поиск ошибок в коде
+
+
